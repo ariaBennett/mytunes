593e9e14a51ae388cc158c9fa033bea364c404fe
Hack Reactor Students <twoplustwo@gmail.com>
2014-03-13 16:42:08 -0700

    Thins is broke but maybe more cool.

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -1,21 +1,23 @@
 // AppView.js - Defines a backbone view class for the whole music app.
 var AppView = Backbone.View.extend({
+  attachRerenderSongQueue: function(){
+    this.model.get('currentSongQueue').on('add', this.currentSongQueueView.render, this.currentSongQueueView);
+    this.model.get('currentSongQueue').on('remove', this.currentSongQueueView.render, this.currentSongQueueView);
+    this.model.get('currentSongQueue').on('add', this.libraryView.render, this.libraryView);
+    this.model.get('currentSongQueue').on('remove', this.libraryView.render, this.libraryView);
+  },
 
   initialize: function(params){
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
-    console.log('in appview init');
-    console.log(this.model.get('currentSongQueue'));
     this.currentSongQueueView = new SongQueueView({collection: this.model.get('currentSongQueue')});
 
     this.model.on('change:currentSong', function(model){
       this.playerView.setSong(model.get('currentSong'));
     }, this);
 
-    this.model.get('currentSongQueue').on('add', this.currentSongQueueView.render, this.currentSongQueueView);
-    this.model.get('currentSongQueue').on('remove', this.currentSongQueueView.render, this.currentSongQueueView);
-    this.model.get('currentSongQueue').on('add', this.libraryView.render, this.libraryView);
-    this.model.get('currentSongQueue').on('remove', this.libraryView.render, this.libraryView);
+
+    this.attachRerenderSongQueue();
   },
 
   render: function(){
