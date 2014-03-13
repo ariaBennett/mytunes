d67a0b0b19e9f2b49707563d6a81cacdcc8de31f
Hack Reactor Students <twoplustwo@gmail.com>
2014-03-12 17:43:54 -0700

    Playlist 80% complete

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -4,16 +4,21 @@ var AppView = Backbone.View.extend({
   initialize: function(params){
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
+    this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
 
     this.model.on('change:currentSong', function(model){
       this.playerView.setSong(model.get('currentSong'));
     }, this);
+
+    this.model.get('songQueue').on('add', this.songQueueView.render, this.songQueueView);
+    this.model.get('songQueue').on('remove', this.songQueueView.render, this.songQueueView);
   },
 
   render: function(){
     return this.$el.html([
       this.playerView.$el,
-      this.libraryView.$el
+      this.libraryView.$el,
+      this.songQueueView.$el
     ]);
   }
 
